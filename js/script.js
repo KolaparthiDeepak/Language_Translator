d1cd3930aec4043c8645d7a0893409285e84391c
deepak <deepakkolaparthi@gmail.com>
2023-08-15 19:02:19 +0530

    added speech button

--- js/script.js
+++ js/script.js
@@ -7,7 +7,13 @@ translateBtn = document.querySelector("button"),
 
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let selected;
+        if (id == 0 && country_code == "en-GB") {
+            selected = "selected";
+        }
+        else if (id == 1 && country_code == "hi-IN") {
+            selected = "selected";
+        }
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
@@ -32,7 +38,9 @@ translateBtn.addEventListener("click", () => {
     let text = fromText.value.trim(),
     translateFrom = selectTag[0].value,
     translateTo = selectTag[1].value;
-    if(!text) return;
+    if(!text) {
+        return;
+    }
     toText.setAttribute("placeholder", "Translating...");
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
     fetch(apiUrl).then(res => res.json()).then(data => {
@@ -48,13 +56,26 @@ translateBtn.addEventListener("click", () => {
 
 icons.forEach(icon => {
     icon.addEventListener("click", ({target}) => {
-        if(!fromText.value || !toText.value) return;
+        if(!fromText.value || !toText.value) {
+            return;
+        }
         if(target.classList.contains("fa-copy")) {
             if(target.id == "from") {
                 navigator.clipboard.writeText(fromText.value);
             } else {
                 navigator.clipboard.writeText(toText.value);
             }
+        }
+        else {
+            let utterance;
+            if(target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance);
         }
     });
 });
